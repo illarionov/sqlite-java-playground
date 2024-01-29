1ab0d8ae6bcfcaf08d25edaf0afc2bab82408ed6
Alexey Illarionov <alexey@0xdc.ru>
2024-01-29 22:11:21 +0300

    GraalVM Wasm Hello World

--- app/src/test/kotlin/org/example/app/MessageUtilsTest.kt
+++ app/src/test/kotlin/org/example/app/MessageUtilsTest.kt
@@ -5,10 +5,7 @@ package org.example.app
 
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.assertEquals
-
 class MessageUtilsTest {
     @Test fun testGetMessage() {
-        assertEquals("Hello      World!", MessageUtils.getMessage())
     }
 }
--- build-logic/src/main/kotlin/buildlogic.kotlin-application-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.kotlin-application-conventions.gradle.kts
@@ -11,3 +11,4 @@ plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
+
