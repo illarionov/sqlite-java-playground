554a4276bbb933ffe9189f21c8c31a2edcc01312
Alexey Illarionov <alexey@0xdc.ru>
2024-01-30 07:04:09 +0300

    sqlite wasm

--- app/src/main/kotlin/org/example/app/App.kt
+++ app/src/main/kotlin/org/example/app/App.kt
@@ -1,26 +1,62 @@
 package org.example.app
 
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
+import kotlin.time.measureTimedValue
+import org.graalvm.polyglot.Context
+import org.graalvm.polyglot.Source
+import org.graalvm.polyglot.Value
 
 private object App
 
-private val wasmContext = Context.newBuilder("wasm")
-    //.allowAllAccess(true)
-    //.option("wasm.Builtins", "wasi_snapshot_preview1")
-    .build()
-private val factorialSource = run {
-    val factorialWasmUrl = requireNotNull(App::class.java.getResource("factorial.wasm"))
-    Source.newBuilder("wasm", factorialWasmUrl).build()
+fun main() {
+    testSqlite()
 }
 
-fun main() {
-    wasmContext.eval(factorialSource)
-    val factorial: Value = wasmContext.getBindings("wasm")
-        .getMember("main")
-        .getMember("fac")
-    println("wasm: factorial(20) = " + factorial.execute(20L))
+private fun testSqlite() {
+    val (sqlite3Initialize, evalDuration) = measureTimedValue {
+        val wasmContext = Context.newBuilder("wasm")
+            .allowAllAccess(true)
+            .option("wasm.Builtins", "wasi_snapshot_preview1")
+            //.option("wasm.Builtins", "emscripten")
+            .build()
+        val sqliteSource = run {
+            //val factorialWasmUrl = requireNotNull(App::class.java.getResource("sqlite3_3450000.wasm"))
+            val factorialWasmUrl = requireNotNull(App::class.java.getResource("sqlite3-wasi-sdk.wasm"))
+            Source.newBuilder("wasm", factorialWasmUrl).build()
+        }
+        wasmContext.eval(sqliteSource)
+
+        val wasmMainBindings = wasmContext.getBindings("wasm")
+
+        println("keys: ${wasmMainBindings.memberKeys}")
+
+        wasmContext.getBindings("wasm")
+            .getMember("main")
+            .getMember("sqlite3_initialize")
+    }
+    println("wasm: binding = $sqlite3Initialize. duration: $evalDuration")
+
+}
+
+private fun testFactorial() {
+    val (factorial, evalDuration) = measureTimedValue {
+        val wasmContext = Context.newBuilder("wasm")
+            //.allowAllAccess(true)
+            //.option("wasm.Builtins", "wasi_snapshot_preview1")
+            .build()
+        val factorialSource = run {
+            val factorialWasmUrl = requireNotNull(App::class.java.getResource("factorial.wasm"))
+            Source.newBuilder("wasm", factorialWasmUrl).build()
+        }
+
+        wasmContext.eval(factorialSource)
+        wasmContext.getBindings("wasm")
+            .getMember("main")
+            .getMember("fac")
+    }
+    val (result, resultDuration) = measureTimedValue {
+        factorial.execute(20L)
+    }
+    println("wasm: factorial(20) = $result. duration: $evalDuration / $resultDuration")
 }
 
 private fun test2() {
