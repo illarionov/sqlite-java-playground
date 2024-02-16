a6b04ac0dc5e056afa9ccea7bd7ff11481497e4c
Alexey Illarionov <alexey@0xdc.ru>
2024-02-17 01:30:12 +0300

    UnsafeMemoryWriter

--- chicory/src/main/kotlin/ru/pixnews/wasm/sqlite3/chicory/wasi/preview1/func/FdWrite.kt
+++ chicory/src/main/kotlin/ru/pixnews/wasm/sqlite3/chicory/wasi/preview1/func/FdWrite.kt
@@ -6,6 +6,7 @@ import com.dylibso.chicory.runtime.Memory
 import com.dylibso.chicory.runtime.WasmFunctionHandle
 import com.dylibso.chicory.wasm.types.Value
 import com.dylibso.chicory.wasm.types.ValueType
+import java.lang.reflect.Field
 import java.nio.ByteBuffer
 import java.util.logging.Level
 import java.util.logging.Logger
@@ -18,9 +19,10 @@ import ru.pixnews.wasm.sqlite3.chicory.host.filesystem.SysException
 import ru.pixnews.wasm.sqlite3.chicory.host.filesystem.model.ReadWriteStrategy
 import ru.pixnews.wasm.sqlite3.chicory.host.filesystem.model.ReadWriteStrategy.CHANGE_POSITION
 import ru.pixnews.wasm.sqlite3.chicory.host.filesystem.model.ReadWriteStrategy.DO_NOT_CHANGE_POSITION
+import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.CioVec
+import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.CiovecArray
 import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.Errno
 import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.Fd
-import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.Iovec
 import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.IovecArray
 import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.Size
 import ru.pixnews.wasm.sqlite3.chicory.wasi.preview1.type.pointer
@@ -54,10 +56,13 @@ private fun fdWrite(
 )
 
 private class FdWrite(
-    private val filesystem: FileSystem,
-    private val strategy: ReadWriteStrategy,
+    filesystem: FileSystem,
+    strategy: ReadWriteStrategy,
     private val logger: Logger = Logger.getLogger(FdWrite::class.qualifiedName)
 ) : WasmFunctionHandle {
+    private val memoryWriter: MemoryWriter = UnsafeMemoryWriter.create(filesystem, strategy)
+        ?: DefaultMemoryWriter(filesystem, strategy)
+
     override fun apply(instance: Instance, vararg args: Value): Array<Value> {
         val fd = Fd(args[0].asInt())
         val pCiov = args[1].asWasmAddr()
@@ -66,10 +71,8 @@ private class FdWrite(
 
         val memory = instance.memory()
         val cioVecs = readCiovecs(memory, pCiov, cIovCnt)
-        val bufs = cioVecs.toByteBuffers(memory)
-
         val errNo = try {
-            val writtenBytes = filesystem.write(fd, bufs, strategy)
+            val writtenBytes = memoryWriter.write(memory, fd, cioVecs)
             memory.writeI32(pNum, writtenBytes.toInt())
             Errno.SUCCESS
         } catch (e: SysException) {
@@ -84,25 +87,88 @@ private class FdWrite(
         memory: Memory,
         pCiov: WasmPtr,
         ciovCnt: Int
-    ): IovecArray {
+    ): CiovecArray {
         val iovecs = MutableList(ciovCnt) { idx ->
             val pCiovec = pCiov + 8 * idx
-            Iovec(
+            CioVec(
                 buf = memory.readI32(pCiovec),
                 bufLen = Size(memory.readI32(pCiovec + 4))
             )
         }
-        return IovecArray(iovecs)
+        return CiovecArray(iovecs)
     }
 
-    private fun IovecArray.toByteBuffers(
-        memory: Memory
-    ): Array<ByteBuffer> = Array(iovecList.size) { idx ->
-        val ciovec = iovecList[idx]
-        val bytes = memory.readBytes(
-            ciovec.buf.asWasmAddr(),
-            ciovec.bufLen.value.asInt()
-        )
-        ByteBuffer.wrap(bytes)
+    private fun interface MemoryWriter {
+        fun write(memory: Memory, fd: Fd, cioVecs: CiovecArray): ULong
     }
+
+    private class UnsafeMemoryWriter private constructor(
+        private val filesystem: FileSystem,
+        private val strategy: ReadWriteStrategy,
+        private val bufferField: Field
+    ) : MemoryWriter {
+
+        override fun write(
+            memory: Memory,
+            fd: Fd,
+            cioVecs: CiovecArray
+        ): ULong {
+            val memoryByteBuffer = bufferField.get(memory) as? ByteBuffer
+                ?: error("Can not get memory byte buffer")
+
+            val bbufs = cioVecs.toByteBuffers(memoryByteBuffer)
+            return filesystem.write(fd, bbufs, strategy)
+        }
+
+        private fun CiovecArray.toByteBuffers(
+            memoryBuffer: ByteBuffer
+        ): Array<ByteBuffer> = Array(ciovecList.size) {
+            val ioVec = ciovecList[it]
+            memoryBuffer.slice(
+                ioVec.buf.asWasmAddr(),
+                ioVec.bufLen.value.asInt()
+            )
+        }
+
+        companion object {
+            fun create(
+                filesystem: FileSystem,
+                strategy: ReadWriteStrategy,
+            ): UnsafeMemoryWriter? {
+                try {
+                    val bufferField = Memory::class.java.getDeclaredField("buffer")
+                    if (!bufferField.trySetAccessible()) {
+                        return null
+                    }
+                    return UnsafeMemoryWriter(filesystem, strategy, bufferField)
+                } catch (nsfe: NoSuchFileException) {
+                    return null
+                } catch (se: SecurityException) {
+                    return null
+                }
+            }
+        }
+    }
+
+    private class DefaultMemoryWriter(
+        private val filesystem: FileSystem,
+        private val strategy: ReadWriteStrategy,
+    ) : MemoryWriter {
+        override fun write(memory: Memory, fd: Fd, cioVecs: CiovecArray): ULong {
+            val bufs = cioVecs.toByteBuffers(memory)
+            return filesystem.write(fd, bufs, strategy)
+        }
+
+        private fun CiovecArray.toByteBuffers(
+            memory: Memory
+        ): Array<ByteBuffer> = Array(ciovecList.size) { idx ->
+            val ciovec = ciovecList[idx]
+            val bytes = memory.readBytes(
+                ciovec.buf.asWasmAddr(),
+                ciovec.bufLen.value.asInt()
+            )
+            ByteBuffer.wrap(bytes)
+        }
+    }
+
 }
